de218c09809797c5a30e66063d8f6095a8bad97a
Takafumi Arakaki <aka.tkf@gmail.com>
2018-01-29 23:12:44 -0800

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
-using EnvInfo
+using EnvInfo: status, save, PkgStatusInfo
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "status" begin
+    @test status("EnvInfo") isa PkgStatusInfo
+    @test all(psi isa PkgStatusInfo for psi in status(["EnvInfo", "JSON"]))
+    @test all(psi isa PkgStatusInfo for psi in status())
+end
+
+@testset "save" begin
+    @test save("/dev/null", ["EnvInfo", "JSON"]) === nothing
+end
